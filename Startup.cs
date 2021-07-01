b2b58b7e703a00c862b684f12ed42a9a836d5859
Sneha <g.snehagopal2001@gmail.com>
2021-07-01 12:12:57 +0530

    UPDATED

--- Startup.cs
+++ Startup.cs
@@ -34,25 +34,24 @@ namespace CityApi
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CityApi", Version = "v1" });
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CityApi v1"));
-                FileServerOptions fileServerOptions = new FileServerOptions();
-                fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
-                fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("index.html");
+
+
+
             }
-            app.UseFileServer(fileServerOptions);
- 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseHttpsRedirection();
+
 
             app.UseRouting();
 
