523b2271fdb8dc1c117aa6a6c7fa2d0754903891
Sneha <g.snehagopal2001@gmail.com>
2021-07-01 16:54:40 +0530

    FINAL

--- wwwroot/City.js
+++ wwwroot/City.js
@@ -2,7 +2,7 @@ var app = new (function () {
     this.fruits = [];
     this.updateindex = 0;
     this.getFruits = function () {
-      fetch("https://snehacityapi.herokuapp.com/api/Fruits")
+      fetch("https://localhost:5001/api/Fruits")
         .then((response) => response.json())
         .then((data) => {
           // console.log(data);
@@ -35,7 +35,7 @@ var app = new (function () {
     this.save = function () {
       document.getElementById("fname").value;
       alert(document.getElementById("fname").value);
-      fetch("https://snehacityapi.herokuapp.com/api/Fruits", {
+      fetch("https://localhost:5001/api/Fruits", {
         method: "post",
         headers: {
           Accept: "application/json",
@@ -67,7 +67,7 @@ var app = new (function () {
           name: document.getElementById("fname").value
       }));
       // PUT to the resource with id = 5 to change the name of task
-  fetch('https://snehacityapi.herokuapp.com/api/Fruits/'+this.updateindex, {
+  fetch('https://localhost:5001/api/Fruits/'+this.updateindex, {
       method: 'PUT',
       body: JSON.stringify({
           id: this.updateindex,
@@ -89,7 +89,7 @@ var app = new (function () {
     
     this.updateindex = this.fruits[i].id;
     alert("delete called " +this.updateindex);
-    fetch('https://snehacityapi.herokuapp.com/api/Fruits/'+this.updateindex, {
+    fetch('https://localhost:5001/api/Fruits/'+this.updateindex, {
     // fetch(`${uri}/${id}`, {
       method: 'DELETE'
     })
